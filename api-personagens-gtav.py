e54d3f4c4e0d2c46fd50ea4b8350b7354cdb4181
Ivan-ReisDev <ivanreisdev@gmail.com>
2022-12-08 19:28:34 -0300

    Atualização, implementação da função de busca por nome

--- api-personagens-gtav.py
+++ api-personagens-gtav.py
@@ -188,18 +188,195 @@ personagens = [
      'foto': 'https://static.wikia.nocookie.net/gta/images/e/e9/Brad-GTAV-prologue.png/revision/latest/scale-to-width-down/350?cb=20161212132013&path-prefix=pt'
      },
 
+    {'id': 22,
+     'nome': 'Tracey De Santa',
+     'negocios': 'Desempregada',
+     'descricao': 'Tracey nasceu em North Yankton por volta de 1991, sendo dois anos mais velha que Jimmy, seu irmão. Lá ela era feliz com sua família mas não se tinha muitas oportunidades em North Yankton, sem contar que seu pai era um ladrão profissional. Após os acontecimentos do O Prólogo, eles se mudam para Los Santos capital de San Andreas. Desde que chegaram lá, a família dos De Santa, tinham ambições, Tracey sonhava em ser famosa, não importava como, Amanda e Michael viviam traindo um ao outro, Michael com prostitutas e Amanda com outros homens, já Jimmy, o irmão mais novo de 20 anos que ainda mora as custas dos pais.',
+     'nacionalidade': 'Americana',
+     'nascido':'1991',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/4/4f/Tracey_De_Santa.png/revision/latest?cb=20131213015731&path-prefix=pt'
+     },
+
+     {'id': 23,
+     'nome': 'Solomon Richards',
+     'negocios': 'Proprietário da Richards Majestic Productions e Diretor de Cinema',
+     'descricao': 'Solomon Richards é um produtor de cinema bem sucedido e proprietário da Richards Majestic Productions desde 1978 uma empresa de cinema que ele herdou de seu pai um clássico produtor de Vinewood chamado David Richards. Ele começou sua carreira produzindo filmes de seu pai, mas depois começou a produzir sozinho e eventualmente ganhou fama na época de 1970. com Filmes aclamados pela crítica nos meados de 1980. Ele teve um relacionamento um pouco complicado com uma das atrizes mais famosas da época Leonora Johnson, ela o considerava um "fake" de seu pai e se recusou a concluir um filme que ele estava a produzir naquele momento. Devido a "diferenças artísticas" e uma grande antipatia por Leonora, eventualmente fez Solomon ser um dos suspeitos pelo assassinato dela, na época. (Mas tarde sabemos que o assassino foi Peter Dreyfuss. mas ele nunca foi acusado devido a falta de provas). No momento em que o jogo acontece, a Richards Majestic Productions está com dificuldades financeiras, após o fracasso econômico de uma de suas produções mais recentes. Cansado da industria de cinema, Solomon tenta recuperar o prestigio de seu estúdio e se aposentar depois de fazer um ultimo filme chamado: Meltdown ',
+     'nacionalidade': 'Americano',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/1/18/SolomonRichards2-GTAV.png/revision/latest?cb=20160823142413&path-prefix=pt'
+     },
+
+     {'id': 24,
+     'nome': 'Lamar Davis',
+     'negocios': 'Tráfico de drogas, Roubo de carros e Fraude de cartão de crédito.',
+     'descricao': 'Lamar cresceu junto a Franklin, Tanisha Jackson e Tonya Wiggins, eles tiveram uma infância difícil e complicada, já no meio de Gangues desde cedo. Lamar sempre teve uma personalidade positiva, sarcástica e nunca era muito inteligente, fazendo alguns de seus planos darem errado, quando de maior, ele começou a se envolver em crimes junto a Franklin.',
+     'nacionalidade': 'Americano',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/6/61/LamarDavis-GTAOe-2021Portrait.png/revision/latest/scale-to-width-down/350?cb=20220107141950&path-prefix=pt'
+     },
+
+     {'id': 25,
+     'nome': 'Wade Hebert',
+     'negocios': 'Vagabundo',
+     'descricao': 'Wade é visto pela primeira vez ao lado de Ron Jakowski tentando acalmar Johnny Klebitz depois que Trevor teve relações sexuais com a namorada de Johnny, Ashley Butler , que terminou com Trevor matando Johnny e iniciando uma guerra entre ele e o The Lost MC; Wade se junta a Trevor em seus ataques contra a gangue. Mais tarde, Trevor pede a Wade que procure Michael Townley , que Wade localiza em Los Santos , após várias tentativas e ameaças fracassadas de Trevor, sob o nome "Michael De Santa". Eles se mudam para o apartamento do primo de Wade, Floyd , para desgosto de Floyd. Wade também ajuda Trevor a explorar um porto em busca de um assalto. Após um incidente no apartamento de Floyd, que resulta na morte de Floyd e de sua namorada,  Debra , Trevor, na tentativa de impedir Wade de descobrir sua morte, afirma que Floyd e Debra "desapareceram", assim como os velhos amigos de Wade, antes de assumir o clube de strip Vanilla Unicorn e pedir dois strippers para assistir a Wade. Wade permanece com eles, mesmo após o enredo principal, e até mantém um diálogo com ele, alegando que cada vez que ele tenta sair, os strippers fazem outra dança.',
+     'nacionalidade': 'Americano',
+     'nascido':'Desconhecido',
+     'foto':'https://static.wikia.nocookie.net/gta/images/f/fc/Wade.png/revision/latest/scale-to-width-down/350?cb=20151022154805&path-prefix=pt'
+     },
+
+     {'id': 26,
+     'nome': 'Devin Weston',
+     'negocios': 'Merryweather Security',
+     'descricao': 'Weston é um bilionário que fez fortuna comprando e vendendo ações, principalmente da Merryweather Security, que foi onde mais lhe deu lucro. Aos poucos ele criou sua própria companhia e começou a investir em outras organizações e atê mesmo em pessoas.',
+     'nacionalidade': 'Americano',
+     'nascido':'1962',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/7/7b/DevinWeston2-GTAV.png/revision/latest?cb=20160903005229&path-prefix=pt'
+     },
+
+     {'id': 27,
+     'nome': 'Dave Norton',
+     'negocios': 'FIB',
+     'descricao': 'David Norton nasceu em um ano desconhecido mas provavelmente no fim dos anos 60, (devido a sua aparência) e se juntou ao FIB antes de 2004. Ele estava nas investigações sobre os roubos, dos criminosos Michael Townley, Trevor Philips e Brad Snider e possivelmente, outros.',
+     'nacionalidade': 'Americano',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/f/f5/Dave_N.png/revision/latest/scale-to-width-down/350?cb=20140814052658&path-prefix=pt'
+     },
+
+     {'id': 28,
+     'nome': 'Steve Haines',
+     'negocios': 'FIB e The Underbelly Of Paradise',
+     'descricao': 'Steve nasceu em 1975 e se juntou a Federal Investigation Bureau (FIB) por volta de 1993. Nos anos seguintes ele foi um agente competente e habilidoso, se destacando entre os demais. Com tudo isso, ele se tornou um agente especial e começou a ser chefe de um trio do FIB, incluindo nele o Agente Andreas Sanchez, que foi um grande amigo de Steve por um tempo, e o agente Dave Norton. Eles eram encarregados de cuidar da Cidade de Los Santos e obedecer as ordens da agência do FIB de lá.',
+     'nacionalidade': 'Americano',
+     'nascido':'1975',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/c/c4/SteveHaines-GTAV.png/revision/latest?cb=20160828160322&path-prefix=pt'
+     },
+
+     {'id': 29,
+     'nome': 'Lacey Jonas',
+     'negocios': 'Atriz e cantora',
+     'descricao': 'Lacey Jonas é muito famosa por seu trabalho em filmes para adolescentes.',
+     'nacionalidade': 'Americana',
+     'nascido':'1990',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/6/67/Lacy_Jonas.png/revision/latest?cb=20180129154941&path-prefix=pt'
+     },
+
+     {'id': 30,
+     'nome': 'Simeon Yetarian',
+     'negocios': 'Premium Deluxe Motorsport',
+     'descricao': 'Simeon é um negociante automotivo de etnia armênia. Sua concessionária, no entanto, é um golpe; ele vende carros para pessoas que não podem pagar por eles e então os recupera por meio de esquemas de financiamento escusos e outros métodos duvidosos. No ar da liberdade de Los Santos, Simeon descobriu como usar sua etnia estrangeira como vantagem nos negócios, acusando quem é contra ele ou simplesmente não quer comprar o veículo de racista.',
+     'nacionalidade': 'Armênio',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/c/cc/Simeon-GTA5.png/revision/latest?cb=20151104143949&path-prefix=pt'
+     },
+
+     {'id': 31,
+     'nome': 'Molly Schultz',
+     'negocios': 'Assessora',
+     'descricao': 'Molly sempre foi ligada ao Direito e se especializou na área, esperando um contrato ela achou e aceitou um trabalho para o grande empreendedor Devin Weston, no qual ela dedicou a carreira assegurando de que seus investimentos e vontades fossem realizados de forma sucetíveis. Desde então, ela nunca abandonou o cliente, até conhecer um trio que estava trabalhando com Devin, Michael De Santa, Trevor Philips e Franklin Clinton.',
+     'nacionalidade': 'Americana',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/3/32/MollySchultz.png/revision/latest?cb=20151224172649&path-prefix=pt'
+     },
+
+     {'id': 32,
+     'nome': 'Martin Madrazo',
+     'negocios': 'Cartel Madrazo',
+     'descricao': 'É o líder de um cartel de drogas chamado Cartel Madrazo, ele é um poderoso e rico empresário estabelecido em Los Santos com sua esposa, Patricia Madrazo. Ele e a esposa, moram em sua fazenda intitulada La Fuente Blanca em Los Santos, e possui uma casa de alto padrão em Vinewood Hills.  Durante a história do GTA V, Michael De Santa destrói a mansão de Madrazo em Vinewood Hills, acreditando que a casa pertencia ao treinador de tênis de Amanda, Kyle Chavis, na missão Terapia de Casais. ',
+     'nacionalidade': 'Mexicano',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/a/a2/MartinMadrazo-GTAV.png/revision/latest?cb=20170124172940&path-prefix=pt'
+     },
+
+     {'id': 33,
+     'nome': 'Tonya Wiggins',
+     'negocios': 'Reboque de Los Santos',
+     'descricao': 'Tonya nasceu e foi criada no Sul de Los Santos, com a filosofia de que "Gangue é família". Na infância seus amigos eram: Franklin Clinton, Tanisha Jackson, Lamar Davis e um garoto que era conhecido como JB. Na adolescência, Tonya começou a namorar JB, juntos eles começaram a mexer com drogas, alguns anos depois, ambos se viciaram em cocaína, crack e maconha. JB, tendo que sustentar a ele e  Tonya, conseguiu um emprego de Rebocador de Carros Roubados, mas com o tempo voltava a se viciar em drogas e isso o atrapalhava no trabalho. Tonya então chama Franklin Clinton, seu antigo amigo de infância para lhes ajudar em alguns reboques na missão Estranhos e Doidos.',
+     'nacionalidade': 'Americana',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/e/ef/TonyaWiggins2-GTAV.png/revision/latest/scale-to-width-down/350?cb=20180628235559&path-prefix=pt'
+     },
+
+     {'id': 34,
+     'nome': 'Isiah Friedlander',
+     'negocios': 'Consultório Friedlander',
+     'descricao': 'Em seu site oficial, Dr. Friedlander é dito ser um psicoterapeuta, personalidade da mídia, budista secular, e que deseja ser um de modelo barcos. Isiah recebeu o grau de bacharel em marketing pela Universidade de San Andreas, Los Santos em 1984 e doutorado em psicologia em 1986 a partir de uma instituição de ensino extinta desconhecida. Ele também tem viajado com freqüência na Ásia . Isiah tem sido amplamente elogiado por expor a cultura do narcisismo nas ganglands de San Andreas e também criticado por seu "uso excessivo de hífens". Ele é uma das maiores autoridades na área de Transtorno de Personalidade Sociopata e já tratou celebridades e criminosos. Isiah tornou-se um dos mais respeitados e requisitados terapeutas em Los Santos a partir de 2013.',
+     'nacionalidade': 'Americano',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/d/da/IsiahFriedlander.png/revision/latest?cb=20151224165727&path-prefix=pt'
+     },
+
+     {'id': 35,
+     'nome': 'Patricia Madrazo',
+     'negocios': 'Mulher do Martin Madrazo',
+     'descricao': 'Pouco se sabe sobre Patricia, além de ser casada com Martin Madrazo, que a maltrata e a trai com uma mulher russa chamada Natalia Zverovna. Ela possui 57 anos de idade de acordo com Trevor. Ela usa um rosário em seu pescoço, o que implica que ela é uma católica romana.',
+     'nacionalidade': 'Mexicana',
+     'nascido':'1956',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/0/09/PatriciaMadrazo-GTAO-2020Portrait.png/revision/latest/scale-to-width-down/350?cb=20201224041339&path-prefix=pt'
+     },
+
+     {'id': 36,
+     'nome': 'John Cranley',
+     'negocios': 'Dublê (Anteriormente) e Candidato a governador',
+     'descricao': 'Cranley tornou-se famoso na década de 1970 quando tinha o seu programa de TV chamado Stunt Double, e mais tarde viria a ser estrela em filmes. Cranley em seu programa advertia contra o uso de drogas e admitia ter tido problemas com drogas no passado. Ele foi preso pela VCPD, sob a acusação de vários crimes na época, embora sendo liberado mais tarde, afirmando que trabalhava como Dublê, e que tudo era autorizado pela polícia em Vice City.',
+     'nacionalidade': 'Americano',
+     'nascido':'1952',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/0/0b/JockCranley-GTAV.png/revision/latest/scale-to-width-down/350?cb=20170322160510&path-prefix=pt'
+     },
+
+     {'id': 37,
+     'nome': 'Ron Jakowski',
+     'negocios': 'Contador (anteriormente), CEO na Trevor Philips Enterprises e Locutor na Blaine County Radio Community Hour Hangares',
+     'descricao': 'Ronald Jakowski nasceu possivelmente no Centro-Oeste dos Estados Unidos e trabalhou anteriormente como contador. Ele era casado antes de seu "melhor amigo" Trevor Philips entrar em sua vida. Trevor o convenceu de que sua esposa o estava retendo apenas na vida. Ron foi expulso por sua esposa e eles se divorciaram, com Ron profundamente endividado, porque ele ainda está pagando taxas de liquidação por ela. Ele então começou a ficar obcecado com as teorias da conspiração e começou a trabalhar para Trevor como CEO da Trevor Philips Enterprises. Em seu programa de rádio, Ron menciona que ele está trabalhando com a tecnologia de interferência de drones.',
+     'nacionalidade': 'Polaco-Americano',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/a/aa/Ron2017.png/revision/latest?cb=20191021131911&path-prefix=pt'
+     },
+
+     {'id': 38,
+     'nome': 'Tao Cheng',
+     'negocios': 'Cheng Family Holdings e Ex-proprietário do The Diamond Casino & Resort',
+     'descricao': 'Nascido em Yangshan, China , Tao é o primogênito de Wei Cheng , o líder das Tríades de Los Santos. Tao fala principalmente em chinês mandarim . Embora ele fale e compreenda o inglês básico, geralmente é acompanhado por seu tradutor . Enquanto o ancião Cheng é implacável e facilmente capaz de retribuir qualquer um que se oponha a ele, Tao provou ser um sucessor da Tríade completamente inepto, exibindo os traços típicos de uma criança mimada. Constantemente se perdendo e sem qualquer senso de iniciativa durante as reuniões de negócios que ele deveria liderar, com o manuseio de seu tradutor. Apesar disso, seu pai se preocupa muito com o filho, apesar de seu comportamento, e nunca o repreende por ir a reuniões de negócios bêbado ou drogado.',
+     'nacionalidade': 'Chinês',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gtawiki/images/8/88/TaoCheng2-GTAO-2019.png/revision/latest/scale-to-width-down/350?cb=20190826164936'
+     },
+
+     {'id': 39,
+     'nome': 'Wei Cheng',
+     'negocios': 'Dono da Gang Los Santos Triads.',
+     'descricao': 'Cheng é um malicioso chinês nascido em Yangshan, que juntamente com o seu filho Tao Cheng, são donos da Gang Los Santos Triads, que lida principalmente com o contrabando de Armas e a fabricação de Metanfetamina. ele conta que em 2013 sua fabricação cresceu fortemente e ele procura expandir seu negocio em Blaine Country através de um grupo já funcionando lá, esse grupo seria o de Trevor, a Trevor Philips Enterprises.',
+     'nacionalidade': 'Chinês',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/b/bd/WeiCheng2-GTAV.png/revision/latest?cb=20161003131720&path-prefix=pt'
+     },
+
+     {'id': 40,
+     'nome': 'Floyd Hebert',
+     'negocios': 'Empregado do Porto de Los Santos',
+     'descricao': 'Floyd sempre foi um homem honesto e nunca quebrou a lei e as regras que lhe eram dadas. Quando mais velho, se casou com uma mulher chamada Debra e juntos, eles foram morar em um apartamento em Vespucci Beach, Los Santos, além de que Debra o ajudou a conseguir um emprego nas docas.',
+     'nacionalidade': 'Americano',
+     'nascido':'Desconhecido',
+     'foto': 'https://static.wikia.nocookie.net/gta/images/c/cb/FloydHebert_ScoutingThePort.png/revision/latest/scale-to-width-down/350?cb=20180717143521&path-prefix=pt'
+     },
+
 ]
 
+
 @app.route('/personagens', methods=['GET'])
-def obter_contas():
+def obter_personagem():
     return jsonify(personagens)
 
 
 @app.route('/personagem/<int:id>', methods=['GET'])
-def obter_contas_por_id(id):
+def obter_personagem_por_id(id):
     for personagem in personagens:
         if personagem.get('id') == id:
             return jsonify(personagem)
 
+@app.route('/personagem/<nome>', methods=['GET'])
+def obter_personagens_por_nome(nome):
+    for personagem in personagens:
+        if personagem.get('nome') == nome:
+            return jsonify(personagem)
 
 app.run(port=5000, host='localhost', debug=True)
